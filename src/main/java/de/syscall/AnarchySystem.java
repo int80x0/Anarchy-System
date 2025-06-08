97e4904eaac9730a8b7301d0ae9764d749e88fe1
80x0 <sieskebusiness@gmail.com>
2025-06-08 17:59:08 +0200

    sum

--- src/main/java/de/syscall/AnarchySystem.java
+++ src/main/java/de/syscall/AnarchySystem.java
@@ -1,11 +1,9 @@
 package de.syscall;
 
 import de.syscall.command.*;
-import de.syscall.listener.DeathListener;
 import de.syscall.listener.PlayerListener;
 import de.syscall.listener.TeleportListener;
 import de.syscall.manager.*;
-import de.syscall.util.MigrationUtility;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AnarchySystem extends JavaPlugin {
@@ -18,8 +16,6 @@ public class AnarchySystem extends JavaPlugin {
     private TeleportManagerCracked teleportManager;
     private TeleportAnimationManagerCracked teleportAnimationManager;
     private HintManager hintManager;
-    private MigrationUtility migrationUtility;
-    private DeathManager deathManager;
 
     @Override
     public void onEnable() {
@@ -27,19 +23,6 @@ public class AnarchySystem extends JavaPlugin {
 
         this.configManager = new ConfigManager(this);
         this.luckPermsManager = new LuckPermsManager(this);
-        this.migrationUtility = new MigrationUtility(this);
-        this.deathManager = new DeathManager(this);
-
-        if (getConfig().getBoolean("migrate-from-uuid", false)) {
-            getLogger().info("Starting migration from UUID to player names...");
-            if (migrationUtility.migrateFromUuidToNames()) {
-                getLogger().info("Migration completed successfully!");
-                getConfig().set("migrate-from-uuid", false);
-                saveConfig();
-            } else {
-                getLogger().warning("Migration failed!");
-            }
-        }
 
         this.homesManager = new HomesManagerCracked(this);
         this.particleManager = new ParticleManagerCracked(this);
@@ -66,9 +49,6 @@ public class AnarchySystem extends JavaPlugin {
         }
         if (teleportAnimationManager != null) {
             teleportAnimationManager.shutdown();
-        }
-        if (deathManager != null) {
-            deathManager.shutdown();
         }
         getLogger().info("Anarchy-System disabled!");
     }
@@ -108,9 +88,6 @@ public class AnarchySystem extends JavaPlugin {
         if (configManager.isModuleEnabled("teleport")) {
             getServer().getPluginManager().registerEvents(new TeleportListener(this), this);
         }
-        if (configManager.isModuleEnabled("death-system")) {
-            getServer().getPluginManager().registerEvents(new DeathListener(this), this);
-        }
     }
 
     public void reload() {
@@ -139,20 +116,11 @@ public class AnarchySystem extends JavaPlugin {
             teleportAnimationManager.shutdown();
         }
 
-        if (deathManager != null) {
-            deathManager.shutdown();
-            this.deathManager = new DeathManager(this);
-        }
-
         getLogger().info("Plugin reload completed!");
     }
 
     public static AnarchySystem getInstance() {
         return instance;
-    }
-
-    public DeathManager getDeathManager() {
-        return deathManager;
     }
 
     public ConfigManager getConfigManager() {
@@ -181,9 +149,5 @@ public class AnarchySystem extends JavaPlugin {
 
     public HintManager getHintManager() {
         return hintManager;
-    }
-
-    public MigrationUtility getMigrationUtility() {
-        return migrationUtility;
     }
 }
